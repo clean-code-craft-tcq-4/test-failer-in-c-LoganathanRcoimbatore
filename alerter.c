fd920e00ba16de81fc11fbfee17a07020824b010
LOGANATHAN <105205462+LoganathanRcoimbatore@users.noreply.github.com>
2022-06-22 16:29:10 +0530

    2 nd update

--- alerter.c
+++ alerter.c
@@ -2,7 +2,6 @@
 #include <assert.h>
 
 #define ALERT_THRESHOLD_LEVEL 200
-
 int alertFailureCount = 0;
 
 int networkAlertStub(float celcius) {
@@ -10,21 +9,26 @@ int networkAlertStub(float celcius) {
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
-        if(celcious < ALERT_THRESHOLD_LEVEL)
-        {           
-         return 200;
-        }
-        else
+         
+         return 200;        
+}
+int Alert_test(float celcius)
+{
+    if (celcius < ALERT_THRESHOLD_LEVEL)
+         {
+             assert(networkAlertStub(celcius)==200);
+         }
+    else
         {
-           return 500;
-        }
-          
+             assert(networkAlertStub(celcius)==500);
+             assert(alertFailureCount != 0);    
+        }  
 }
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlertStub(celcius);
-
+   
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
@@ -32,14 +36,13 @@ void alertInCelcius(float farenheit) {
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
-
+   Alert_test(celcius);
+  
 }
 
 int main() {
-    alertInCelcius(400.5);
-        assert(alertFailureCount == 1);
+    alertInCelcius(400.5); 
     alertInCelcius(303.6);
-         assert(alertFailureCount == 0);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
